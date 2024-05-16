6c668a42b6a043076c3f235867eb35d1acd9d8ae
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-15 20:30:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const findId = document.getElementById('text')
+     findId.innerHTML = 'This is really cool!'
+})
